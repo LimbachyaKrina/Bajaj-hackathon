81969d9d9b93ca0bf8a8509a331ba249d0a60756
Krina <148012449+LimbachyaKrina@users.noreply.github.com>
2025-08-01 01:38:06 +0530

    Update build.sh for dp

--- build.sh
+++ build.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 apt-get update
 apt-get install -y poppler-utils libblas-dev liblapack-dev rustc cargo
+export CARGO_HOME=/tmp/cargo
+export RUSTUP_HOME=/tmp/rustup
+pip install cryptography --no-build-isolation
 pip install -r requirements.txt
